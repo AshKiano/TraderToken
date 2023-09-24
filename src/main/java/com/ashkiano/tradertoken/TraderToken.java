d90c1abdbfa2ba49d415404d73814c0106a90278
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2023-09-24 21:00:59 +0200

    version 1.2

--- src/main/java/com/ashkiano/tradertoken/TraderToken.java
+++ src/main/java/com/ashkiano/tradertoken/TraderToken.java
@@ -23,6 +23,8 @@ public class TraderToken extends JavaPlugin implements Listener {
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
         Metrics metrics = new Metrics(this, 19515);
+
+        this.getLogger().info("Thank you for using the TraderToken plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
     }
 
     @Override
