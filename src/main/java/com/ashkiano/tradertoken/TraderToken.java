797ca5b2cf7c546c361430157995fa2edf871942
AshKiano <ash.kiano@gmail.com>
2023-09-29 21:39:55 +0200

    version 1.3

--- src/main/java/com/ashkiano/tradertoken/TraderToken.java
+++ src/main/java/com/ashkiano/tradertoken/TraderToken.java
@@ -2,6 +2,7 @@ package com.ashkiano.tradertoken;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,14 +14,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class TraderToken extends JavaPlugin implements Listener {
 
-    private final String TOKEN_LORE = "Special token to summon a wandering trader";
+    private String commandPermission;
+    private String itemName;
+    private List<String> itemLore;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+        commandPermission = config.getString("commandPermission", "tradertoken.givetoken");
+        itemName = config.getString("itemName", "Trader Token");
+        itemLore = config.getStringList("itemLore");
+
         Bukkit.getPluginManager().registerEvents(this, this);
         Metrics metrics = new Metrics(this, 19515);
     }
@@ -28,15 +37,19 @@ public class TraderToken extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("givetoken")) {
-            if (args.length == 1) {
-                Player target = Bukkit.getPlayer(args[0]);
-                if (target != null) {
-                    giveToken(target);
-                    sender.sendMessage("Token has been given to " + target.getName());
-                } else {
-                    sender.sendMessage("Player is not online.");
+            if (sender.hasPermission(commandPermission)) {
+                if (args.length == 1) {
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target != null) {
+                        giveToken(target);
+                        sender.sendMessage("Token has been given to " + target.getName());
+                    } else {
+                        sender.sendMessage("Player is not online.");
+                    }
+                    return true;
                 }
-                return true;
+            } else {
+                sender.sendMessage("You don't have permission to use this command.");
             }
         }
         return false;
@@ -47,7 +60,7 @@ public class TraderToken extends JavaPlugin implements Listener {
         ItemStack item = event.getItem();
         if (item != null && item.getType() == Material.EMERALD) {
             ItemMeta meta = item.getItemMeta();
-            if (meta != null && meta.hasLore() && meta.getLore().contains(TOKEN_LORE)) {
+            if (meta != null && meta.hasLore() && meta.getLore().equals(itemLore)) {
                 event.getPlayer().getWorld().spawnEntity(event.getPlayer().getLocation(), EntityType.WANDERING_TRADER);
                 if(item.getAmount() > 1) {
                     item.setAmount(item.getAmount() - 1);
@@ -63,8 +76,8 @@ public class TraderToken extends JavaPlugin implements Listener {
         ItemStack token = new ItemStack(Material.EMERALD, 1);
         ItemMeta meta = token.getItemMeta();
         if (meta != null) {
-            meta.setDisplayName("Trader Token");
-            meta.setLore(Arrays.asList(TOKEN_LORE));
+            meta.setDisplayName(itemName);
+            meta.setLore(itemLore);
             token.setItemMeta(meta);
             player.getInventory().addItem(token);
         }
